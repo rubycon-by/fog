05fd6032e5d0a07b2076e57aaeaf18c116943470
Oleg Shappo <humanresourcett@gmail.com>
2013-04-30 19:12:09 +0300

    added iso/isos models

--- lib/fog/cloudstack/models/compute/isos.rb
+++ lib/fog/cloudstack/models/compute/isos.rb
@@ -9,20 +9,21 @@ module Fog
 
         model Fog::Compute::Cloudstack::Iso
 
-        def all
-          data = service.list_isos["listisosresponse"]["iso"] || []
+        def all(attributes={})
+          response = service.list_isos
+          data = response["listisosresponse"]["iso"] || []
           load(data)
         end
 
         def get(iso_id)
-          if iso = service.list_isos["listisosresponse"]["iso"].first
+          if iso = service.list_isos('id' => iso_id)["listisosresponse"]["iso"].first
             new(iso)
           end
         rescue Fog::Compute::Cloudstack::BadRequest
           nil
         end
-
       end
+
     end
   end
 end
