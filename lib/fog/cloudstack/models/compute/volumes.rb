69b92255af3cf13ee1f84137aca3eda698f2c8e1
Oleg Shappo <humanresourcett@gmail.com>
2013-05-02 19:15:04 +0300

    added template requests

--- lib/fog/cloudstack/models/compute/volumes.rb
+++ lib/fog/cloudstack/models/compute/volumes.rb
@@ -5,23 +5,23 @@ module Fog
   module Compute
     class Cloudstack
 
-      class Volumes < Fog::Collection
+      # class Volumes < Fog::Collection
 
-        model Fog::Compute::Cloudstack::Volume
+      #   model Fog::Compute::Cloudstack::Volume
 
-        def all
-          data = service.list_volumes["listvolumesresponse"]["volume"] || []
-          load(data)
-        end
+      #   def all
+      #     data = service.list_volumes["listvolumesresponse"]["volume"] || []
+      #     load(data)
+      #   end
 
-        def get(volume_id)
-          if volume = service.list_volumes('id' => volume_id)["listvolumesresponse"]["volume"].first
-            new(volume)
-          end
-        rescue Fog::Compute::Cloudstack::BadRequest
-          nil
-        end
-      end
+      #   def get(volume_id)
+      #     if volume = service.list_volumes('id' => volume_id)["listvolumesresponse"]["volume"].first
+      #       new(volume)
+      #     end
+      #   rescue Fog::Compute::Cloudstack::BadRequest
+      #     nil
+      #   end
+      # end
 
     end
   end
