28a2903a6dcfe6ed2fc643a94c4193701000f6ac
Oleg Shappo <humanresourcett@gmail.com>
2013-05-14 23:27:22 +0300

    collection change

--- lib/fog/core/service.rb
+++ lib/fog/core/service.rb
@@ -91,8 +91,7 @@ module Fog
             constant = collection.to_s.split('_').map {|characters| characters[0...1].upcase << characters[1..-1]}.join('')
             service::Collections.module_eval <<-EOS, __FILE__, __LINE__
               def #{collection}(attributes = {})
-                p 'service ips'
-                #{service}::#{constant}.new({:service => self}.merge(attributes))
+                p {:service => self}.merge(attributes)
                 #{service}::#{constant}.new({:service => self}.merge(attributes))
               end
             EOS
