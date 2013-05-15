df6ad8cc5b684ff73e2c621193077d447fe22f90
Oleg Shappo <humanresourcett@gmail.com>
2013-05-15 14:38:32 +0300

    collection change

--- lib/fog/cloudstack/models/compute/ips.rb
+++ lib/fog/cloudstack/models/compute/ips.rb
@@ -10,15 +10,13 @@ module Fog
         model Fog::Compute::Cloudstack::Ip
 
         def all(attributes={})
-          p @filter_attributes
-          response = service.list_public_ip_addresses @filter_attributes.merge!(attributes)
+          response = service.list_public_ip_addresses scoped_attributes(attributes)
           data = response["listpublicipaddressesresponse"]["publicipaddress"] || []
           load(data)
         end
 
         def get(ip_id)
-          p @filter_attributes
-          response = service.list_public_ip_addresses({id: ip_id}.merge!(@filter_attributes){|key, new_value, old_value| new_value != old_value ? -1 : new_value })
+          response = service.list_public_ip_addresses(scoped_attributes({id: ip_id})
           if ip = response["listpublicipaddressesresponse"]["publicipaddress"].try(:first)
             new(ip)
           end
@@ -29,7 +27,7 @@ module Fog
         protected
 
         def scoped_attributes attributes = {}
-          attributes.merge!(@filter_attributes){|key, new_value, old_value| new_value != old_value ? -1 : new_value }
+          attributes{id: ip_id}.merge!(@filter_attributes){|key, new_value, old_value| new_value != old_value ? -1 : new_value }
         end
 
       end
