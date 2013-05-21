80f10c7f0a4bd7c8847b97dce6b4fcff4420cd3a
Oleg Shappo <humanresourcett@gmail.com>
2013-05-21 13:10:06 +0300

    collection

--- lib/fog/cloudstack/models/compute/ips.rb
+++ lib/fog/cloudstack/models/compute/ips.rb
@@ -14,7 +14,7 @@ module Fog
           response = service.list_public_ip_addresses attributes
           data = response["listpublicipaddressesresponse"]["publicipaddress"] || []
           clear
-          data.map{|x| new(x)}
+          data.map{|x| new(x)} & self.to_a
         end
 
         def get(ip_id)
