a6f6530079195028f8cd88edcaf5bb127ce7f8ee
Oleg Shappo <humanresourcett@gmail.com>
2013-05-28 11:41:59 +0300

    collection

--- lib/fog/cloudstack/models/compute/ips.rb
+++ lib/fog/cloudstack/models/compute/ips.rb
@@ -12,9 +12,9 @@ module Fog
         def all(attributes={})
           response = service.list_public_ip_addresses scoped_attributes(attributes)
           data = response["listpublicipaddressesresponse"]["publicipaddress"] || []
-          # condition = @filter_attributes.nil?
-          # @filter_attributes = attributes.except("command", "response", "sessionkey") if @filter_attributes.nil?
-          load(data)
+          condition = @filter_attributes.nil?
+          @filter_attributes = attributes.except("command", "response", "sessionkey") if @filter_attributes.nil?
+          load(data, condition)
         end
 
         def get(ip_id)
